9cad189b3dfd4e1641420fc1f7d02778cde049a0
Viral Patel <exc.viral@gmail.com>
2018-09-12 00:28:16 +0530

    Implemented uniform crossover operator function

--- binary-coded genetic algorithm/crossover.py
+++ binary-coded genetic algorithm/crossover.py
@@ -38,7 +38,7 @@ def unique_rn_generator(low, high, n):
 
 def kp_crossover(parent1, parent2, k):
     """
-    This function is implementation of "k-point crossover"
+    This function is implementation of "k-point crossover" of genetic algorithm
 
     Algorithm:
     --[1] Generate k unique random numbers between 0 and n, where n is the number of genes in parents.
@@ -60,8 +60,8 @@ def kp_crossover(parent1, parent2, k):
     """
     r = unique_rn_generator(1, len(parent1), k)
     print(r)
-    c1 = []
-    c2 = []
+    c1 = bitarray()
+    c2 = bitarray()
     switch = False
 
     for i in range(len(parent1)):
@@ -74,4 +74,33 @@ def kp_crossover(parent1, parent2, k):
         elif (switch == True):
             c1.append(parent2[i])
             c2.append(parent1[i])
-    return [bitarray(c1), bitarray(c2)]
+    return [c1, c2]
+
+
+def uniform_crossover(parent1, parent2):
+    """
+    This function is implementation of "uniform crossover" operator of genetic algorithm
+
+    In uniform crossover, bits are randomly copied from the first or from the second parent, and two children are
+    generated.
+
+    Algorithm:
+    --[1] Generate a mask of length same as number of genes in parents, mask will contain 0 or 1 at random positions
+    --[2] Swap the gene values of parents for positions where there are 1's in mask, and do nothing where there are 0's
+          in the mask.
+
+    :param parent1: (bitarray) containing genes of parent 1
+    :param parent2: (bitarray) containing genes of parent 2
+    :return: (list of bitarrays) containing 2 children of parent 1 and parent 2
+    """
+    mask = np.random.randint(0,2,len(parent1))
+    c1 = bitarray()
+    c2 = bitarray()
+    for i in range(len(mask)):
+        if mask[i] == 1 :
+            c1.append(parent2[i])
+            c2.append(parent1[i])
+        else :
+            c1.append(parent1[i])
+            c2.append(parent2[i])
+    return [c1,c2]
