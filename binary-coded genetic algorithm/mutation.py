37d7a2825585e0f7922edd390d1fc483af27e837
Viral Patel <exc.viral@gmail.com>
2018-09-11 22:29:49 +0530

    Implemented mutation algorithm for binary-coded genetic algorithm

---
+++ binary-coded genetic algorithm/mutation.py
@@ -0,0 +1,31 @@
+# Mutation is a genetic operator used to maintain genetic diversity from one generation of a population of chromosomes
+# to the next. It is analogous to biological mutation.
+
+# import necessary libraries
+
+from bitarray import bitarray
+import numpy as np
+
+# Bit reversal mutation
+# Generate a random number between 0 and 1, multiply it with number of genes in the chromosome, take integer part of it
+# and select that index in the chromosome, and flip the bit value at that index.
+
+
+def br_mutation(chromosome):
+    """
+    Function to perform bit reversal mutation in a given chromosome
+
+    Algorithm:
+    --[1] Generate a uniform random number 'r' between 0 and 1
+    --[2] Multiply 'r' with number of genes in the chromosome, and take the integer part of it, i.e. drop the decimal
+          part of it.
+    --[3] Select 'rth' gene from chromosome, and flip/invert its value
+
+    :param chromosome: (bitarray) of bits containing some number of genes, representing a chromosome
+    :return: (bitarray) containing mutated chromosome
+    """
+    r = int((np.random.uniform())*len(chromosome))
+    print(r)
+    chromosome[r] = not(chromosome[r])
+    print(chromosome)
+    return chromosome
