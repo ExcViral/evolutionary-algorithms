e51184e4b3827fd8ef9fc93ea6285a4feb27b261
Viral Patel <exc.viral@gmail.com>
2018-09-13 06:57:22 +0530

    Added template for fitness evaluation function

---
+++ binary-coded genetic algorithm/fitness.py
@@ -0,0 +1,29 @@
+# A fitness function quantifies the optimality of a solution (chromosome) so that that particular solution may be
+# ranked against all the other solutions.
+# It depicts the closeness of a given ‘solution’ to the desired result.
+# NOTE: Watch out for its speed.
+
+# Most functions are stochastic and designed so that a small proportion of less fit solutions are selected. This helps
+# keep the diversity of the population large, preventing premature convergence on poor(local) solutions.
+
+# NOTE: Fitness functions will vary from application to application, so for each different application, you will have to
+#       define a new fitness function by looking at the goal, and the constraints of the application. This file is a
+#       template for implementing the same.
+
+# import necessary libraries and modules
+
+
+# ======================================================================================================================
+# ===== Helper functions ===============================================================================================
+# ======================================================================================================================
+
+# Implement helper functions here (if any)
+
+
+# ======================================================================================================================
+# ===== Fitness evaluation function ====================================================================================
+# ======================================================================================================================
+
+def fitness(population):
+    # Implement the fitness evaluation function here
+    return fitness
